607bd78888cae8de52932edb3275ae4de01e8853
Adam <adammonast@Adams-MBP.attlocal.net>
2020-01-15 19:27:29 -0500

    submitting project w/o stretch

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,14 @@
+import useLocalStorage from "./useLocalStorage";
+import {useEffect} from "react";
+
+export const useDarkMode = (key, initialValue) => {
+  const [darkMode, setDarkMode] = useLocalStorage(key, initialValue);
+  useEffect(() => {
+    if (!darkMode) {
+      document.querySelector("body").classList.remove("dark-mode");
+    } else {
+      document.querySelector("body").classList.add("dark-mode");
+    }
+  }, [darkMode]);
+  return [darkMode, setDarkMode];
+};
